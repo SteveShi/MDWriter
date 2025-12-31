3a033eb9b2ffb04733353339ac33dbaf96334560
Steve Shi <appleshiyu@outlook.com>
2025-12-31 17:41:54 +0800

    Initial Commit

---
+++ MDWriter/MDWriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MDWriterApp.swift
+//  MDWriter
+//
+//  Created by 石屿 on 2025/12/31.
+//
+
+import SwiftUI
+
+@main
+struct MDWriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MDWriterTests/MDWriterTests.swift
@@ -0,0 +1,16 @@
+//
+//  MDWriterTests.swift
+//  MDWriterTests
+//
+//  Created by 石屿 on 2025/12/31.
+//
+
+import Testing
+
+struct MDWriterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
