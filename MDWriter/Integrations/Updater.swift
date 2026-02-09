48025d84b4eb41f6543f75efb4f106843f7fe550
Steve Shi <xuanlian@mac.com>
2026-02-09 14:45:14 +0800

    Fix Sparkle updater concurrency

--- MDWriter/Integrations/Updater.swift
+++ MDWriter/Integrations/Updater.swift
@@ -11,6 +11,7 @@ import SwiftUI
 import Combine
 
 /// A wrapper around Sparkle's SPUStandardUpdaterController
+@MainActor
 class Updater: ObservableObject {
     private let updaterController: SPUStandardUpdaterController
     
